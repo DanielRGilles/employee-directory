333282d152a5effdf20652f14d69028838d142e6
DanielRGilles <danryangilles@gmail.com>
2022-01-05 15:43:25 -0800

    building out basic structure and importing services folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,33 @@
+import React from 'react'
+import { UserProvider } from './context/UserContext';
+import Auth from './views/Auth/Auth';
+import Home from './views/Home/Home';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <UserProvider>
+        <Router>
+          <Header />
+          <main className={styles.main}>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/login">
+                <Auth />
+              </Route>
+              <Route path="/register">
+                <Auth isSigningUp />
+              </Route>
+              <Route path="">
+              </Route>
+              <PrivateRoute path="">
+              </PrivateRoute>
+             
+            </Switch>
+          </main>
+        </Router>
+     
+    </UserProvider>
+  )
 }
